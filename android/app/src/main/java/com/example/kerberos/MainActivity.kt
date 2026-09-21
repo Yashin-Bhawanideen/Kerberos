acb9e9ae1562db4ba709e99b1d8e7e6f86316e71
Mishal Bhikha <st10450208@myemeris.edu.za>
2026-09-22 01:16:32 +0200

    saved language on app startup

--- android/app/src/main/java/com/example/kerberos/MainActivity.kt
+++ android/app/src/main/java/com/example/kerberos/MainActivity.kt
@@ -11,17 +11,34 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kerberos.ui.KerberosNavGraph
 import com.example.kerberos.ui.theme.KerberosTheme
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+import com.example.kerberos.settings.SettingsManager
 
-class MainActivity : FragmentActivity() {
-
+class MainActivity : AppCompatActivity() {
     companion object {
         private const val NOTIFICATION_PERMISSION_REQUEST_CODE = 100
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //loads the language that the user selected
+        val settingsManager = SettingsManager(this)
+        val savedLanguage = settingsManager.getLanguage()
+
+        val languageCode = if (savedLanguage == "Afrikaans") {
+            "af"
+        } else {
+            "en"
+        }
+
+        //applies the saved app language (Android Developers, 2026)
+        AppCompatDelegate.setApplicationLocales(
+            LocaleListCompat.forLanguageTags(languageCode)
+        )
+
         super.onCreate(savedInstanceState)
 
         requestNotificationPermission()
@@ -54,3 +71,11 @@ class MainActivity : FragmentActivity() {
         }
     }
 }
+
+/*
+REFERENCE LIST
+
+Android Developers. 2026. Per-app language preferences. [Online].
+Available at: https://developer.android.com/guide/topics/resources/app-languages
+[Accessed 22 September 2026].
+*/
